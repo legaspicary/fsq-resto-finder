f4df636b86f9a54ea35d7b6942441e8411f1448c
Cary Legaspi <legaspicary@gmail.com>
2025-09-11 00:10:03 +0800

    implement custom fetch for places search

--- src/foursquare/places-search.ts
+++ src/foursquare/places-search.ts
@@ -3,25 +3,45 @@ import * as dotenv from 'dotenv'
 import type { FourSquareQuerySchema } from '../schemas/FourSquareQuerySchema.js';
 import type z from 'zod';
 import { FourSquareResultSchema } from '../schemas/FourSquareResultSchema.js';
-import * as fsqDevelopersPlaces from "@api/fsq-developers-places";
 
 dotenv.config();
 
-const FSQ_DEFAULT_CONFIG = {
-    // category for restaurants in their latest api
+const FSQ_DEFAULT_HEADERS = {
     fsq_category_ids: "4d4b7105d754a06374d81259",
+    accept: 'application/json',
+}
+
+const DEFAULT_HEADERS = {
     'X-Places-Api-Version': '2025-06-17' as const
+}
+
+const placesSearch = async (query: z.infer<typeof FourSquareQuerySchema>, apiToken: string) => {
+    const stringifiedParams = JSON.stringify({
+        ...query.parameters,
+        ...FSQ_DEFAULT_HEADERS,
+    })
+    const params: Record<string, string> = JSON.parse(stringifiedParams)
+    const urlParams = new URLSearchParams(params);
+    const fsqUrl = "https://places-api.foursquare.com/places/search?" + urlParams.toString();
+    const res = await fetch(fsqUrl, {
+        method: "GET",
+        headers: { ...DEFAULT_HEADERS, authorization: "Bearer " + apiToken }
+    })
+
+    if (res.status !== 200) {
+
+        throw new Error("Failed to get the API")
+    }
+
+    const jsonRes = await res.json()
+    const parsedResults = FourSquareResultSchema.parse(jsonRes)
+    return parsedResults
 }
 
 export async function lookupRestaurant(query: z.infer<typeof FourSquareQuerySchema>): Promise<z.infer<typeof FourSquareResultSchema>> {
     const fsqApiKey = process.env.FSQ_PLACES_API_KEY
     if (!fsqApiKey) throw new Error("FSQ API KEY is not defined!");
 
-    fsqDevelopersPlaces.auth('Z531SUQ4PKW5WGEED4GNNWWHM0M5YHEZ23Z1KXNDOTTEYSU5');
-    const results = fsqDevelopersPlaces.placeSearch({
-        ...query.parameters,
-        ...FSQ_DEFAULT_CONFIG,
-    })
-    const parsedResults = FourSquareResultSchema.parse(results)
-    return parsedResults
+    const results = await placesSearch(query, fsqApiKey)
+    return results
 }
