124928dee2d85dc12b41321f84969ce4c60fd778
Cary Legaspi <legaspicary@gmail.com>
2025-09-10 21:14:58 +0800

    run dummy agent

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,25 @@
 import express from 'express'
 // TODO: why need .js
 import { ExecuteApiQuerySchema } from './schemas/ExecuteApiSchema.js'
+import { runAgent } from './langgraph/agent.js';
 
 const app = express()
 
 // TODO: check code in query is pioneerdevai
-app.get('/api/execute', (req, res) => {
+app.get('/api/execute', async (req, res) => {
   const parsedQuery = ExecuteApiQuerySchema.parse(req.query);
-  if(parsedQuery.code !== 'pioneerdevai') {
+  if (parsedQuery.code !== 'pioneerdevai') {
     res.sendStatus(401);
     return;
   }
 
+  await runAgent();
+
   // TODO: add llm layer
 
   // TODO: add foursquare layer
-  
-  res.send('Hello World')
+
+  res.send('Hello Worlda')
 })
 
 app.listen(3000, () => {
