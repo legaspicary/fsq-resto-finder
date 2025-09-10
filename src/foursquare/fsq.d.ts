df203bc6f6974220e906dd1fd6af2fcbb550c380
Cary Legaspi <legaspicary@gmail.com>
2025-09-10 22:03:53 +0800

    generate manual type for fsq lib

---
+++ src/foursquare/fsq.d.ts
@@ -0,0 +1,13 @@
+declare module "@api/fsq-developers-places" {
+    export function auth(authToke: string): void;
+    export function placeSearch(search: {
+        query: string,
+        fsq_category_ids: string,
+        price: number,
+        open_now: boolean,
+        rating: number,
+        near: string,
+        'X-Places-Api-Version': '2025-06-17'
+    })
+    export const version: string;
+} expor
